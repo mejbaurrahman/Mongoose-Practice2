0439df339b7a0986ea67245c5febb4a2374edf25
unknown <mejbacsedu@gmail.com>
2024-06-28 13:03:48 +0600

    eslint install

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,17 @@
+import app from "./app";
+import config from "./app/config";
+
 const mongoose = require("mongoose");
 main().catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect(
-    `mongodb+srv://<username>:<password>@cluster0.xvker.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-  );
+  try {
+    await mongoose.connect(config.database_url as string);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`Example app listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
